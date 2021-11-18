7b94f6fb1d900848679e8fe10b916a04b88c2421
Oscar Rodriguez <00206019@uca.edu.sv>
2021-11-17 20:00:32 -0500

    Add: print function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import './App.css';
 import Axios from 'axios';
 
 function App() {
   const [productName, setProductName]= useState("");
   const [productQuantity, setProductQuantity]=useState(0);
+
+const [productList, setproductList]= useState([]);
+
+  //obtenemos la informacion de la bd una vez
+  useEffect(()=>{
+    Axios.get("http://localhost:3000/read").then((response)=>{
+      setproductList(response.data);
+    })
+  },[]);
 
   const addToList=()=>{
     Axios.post("http://localhost:3000/insert",{productName:productName,productQuantity:productQuantity});
@@ -17,6 +26,16 @@ function App() {
     <label>Cantidad del producto</label>
     <input type="number"  onChange={(event)=>{setProductQuantity(event.target.value)}}/>
     <button onClick={addToList}>AGREGAR</button>
+
+    <h1>Productos en BD</h1>
+    {productList.map((val,key)=>{
+      return(
+      <div key={key}>
+        <h1>{val.productName}</h1>
+        <h1>{val.productQuantity}</h1>
+        </div>
+        );
+    })}
     </div>
   );
 }
